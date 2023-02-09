db74834dacf190e34f750c7908b3d9be66047293
Kalhong Narod <kalhn443@entro.postbox.in.th>
2023-02-09 16:03:24 +0700

    edit log

--- src/routes.js
+++ src/routes.js
@@ -2,23 +2,19 @@
 module.exports = (app) => {
 
     app.get('/api/all', (req, res) => {
-        console.log(req.body)
         res.send(require('./res/get-all.json'));
     });    
     
 
     app.get('/api/:keyName/:keyValue.(json)', (req, res) => {
-        console.log(req.body)
         res.send(require('./res/get-one.json'));
     });   
  
     app.post('/api/post', (req, res) => {
-        console.log(req.body)
         res.send(require('./res/post.json'));
     });  
  
     app.all('*', (req, res) => {
-        console.log(req.body)
         res.send(require('./res/all.json'));
     });
 
